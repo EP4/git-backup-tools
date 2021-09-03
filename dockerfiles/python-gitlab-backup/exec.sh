d652f9dafb27b6f5f29bb589daaef4efa9908505
Samuel Hautcoeur <samuel.hautcoeur@ep4.com>
2021-09-03 14:32:29 +0000

    Initial commit

---
+++ dockerfiles/python-gitlab-backup/exec.sh
@@ -0,0 +1,24 @@
+  
+#!/bin/sh
+
+TIME_ZONE=${TIME_ZONE:=UTC}
+echo "timezone=${TIME_ZONE}"
+cp /usr/share/zoneinfo/${TIME_ZONE} /etc/localtime
+echo "${TIME_ZONE}" >/etc/timezone
+
+echo "$(date) - start backup scheduler"
+while :; do
+    DATE=$(date +%Y%m%d-%H%M%S)
+
+    for u in $(echo $GITLAB_USER | tr "," "\n"); do
+        echo "$(date) - execute backup for ${u}, ${DATE}"
+        gitlab-backup ${u} --host=$HOST --private-token=$TOKEN --all --output-directory=/srv/var/${DATE}/${u} --private --gists
+    done
+
+    echo "$(date) - cleanup"
+
+    ls -d1 /srv/var/* | head -n -${MAX_BACKUPS} | xargs rm -rf
+
+    echo "$(date) - sleep for 1 day"
+    sleep 1d
+done
